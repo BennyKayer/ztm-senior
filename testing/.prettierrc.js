25d6b66bc33f3d61e338e1ba4a60cd196b29d61e
Paweł Benkowski <pawlo97.pb@gmail.com>
2021-05-05 14:26:55 +0200

    Test setup vid 170

---
+++ testing/.prettierrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+    printWidth: 80,
+    singleQuote: false,
+    useTabs: false,
+    tabWidth: 4
+}
---
+++ testing/script.js
@@ -0,0 +1,17 @@
+const googleDatabase = [
+    "cats.com",
+    "souprecipes.com",
+    "flowers.com",
+    "animals.com",
+    "catpics.com",
+    "mycatblog.com",
+];
+
+const googleSearch = (searchInput) => {
+    const matches = googleDatabase.filter((website) => {
+        return website.includes(searchInput);
+    });
+    return matches.length > 3 ? matches.slice(0, 3) : matches;
+};
+
+console.log(googleSearch("soup"));
