1c61c343adb9fb951e26b4f3611196618aac1cda
Paweł Benkowski <pawlo97.pb@gmail.com>
2021-05-06 13:51:04 +0200

    vid 171

--- testing/script.js
+++ testing/script.js
@@ -7,11 +7,12 @@ const googleDatabase = [
     "mycatblog.com",
 ];
 
-const googleSearch = (searchInput) => {
-    const matches = googleDatabase.filter((website) => {
+const googleSearch = (searchInput, db) => {
+    const matches = db.filter((website) => {
         return website.includes(searchInput);
     });
     return matches.length > 3 ? matches.slice(0, 3) : matches;
 };
 
-console.log(googleSearch("soup"));
+console.log(googleSearch("soup", googleDatabase));
+module.exports = googleSearch;
--- testing/script.test.js
+++ testing/script.test.js
@@ -1,1 +1,15 @@
-it("this is a test", () => {});
+const googleSearch = require("./script");
+
+dbMock = ["dog.com", "cheesepuff.com", "disney.com", "dogpictures.com"];
+
+it("silly test", () => {
+    expect("hello").toBe("hello");
+});
+
+it("no matches test", () => {
+    expect(googleSearch("jebać czkawki", dbMock)).toEqual([]);
+});
+
+it("is searching google", () => {
+    expect(googleSearch("dog", dbMock)).toEqual(["dog.com", "dogpictures.com"]);
+});
