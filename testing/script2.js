aa4a8d423d62ea04d04e03ab9f31a5013a91f94c
Paweł Benkowski <pawlo97.pb@gmail.com>
2021-05-12 21:25:17 +0200

    175. Asynchronous Tests 2

--- testing/script2.js
+++ testing/script2.js
@@ -4,7 +4,7 @@ const getPeoplePromise = (fetch) => {
     return fetch("https://swapi.dev/api/people/")
         .then((response) => response.json())
         .then((data) => {
-            console.log(data);
+            // console.log(data);
             return {
                 count: data.count,
                 results: data.results,
@@ -15,7 +15,7 @@ const getPeoplePromise = (fetch) => {
 const getPeople = async (fetch) => {
     const getRequest = await fetch("https://swapi.dev/api/people/");
     const data = await getRequest.json();
-    console.log(data);
+    // console.log(data);
     return {
         count: data.count,
         results: data.results,
