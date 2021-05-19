2431f00846272098fa34f2fda012530be2d22a0e
Paweł Benkowski <pawlo97.pb@gmail.com>
2021-05-19 12:48:53 +0200

    184. Snapshot Testing

--- section_6_testing/testing_robofriends/README.md
+++ section_6_testing/testing_robofriends/README.md
@@ -11,3 +11,11 @@ To run the project:
 # Enzyme
 
 1. Start with creating setupTests
+2. Fix empty snapshots with jest config in package.json
+3. Snapshot is taken at test run
+4. To tell tests "hey this change is intentional" you need to press w to show more then u to retake snapshot
+
+# Commands
+
+`npm test -- --coverage` - not working
+`npm test -- --coverage --watchAll=false` - working
--- section_6_testing/testing_robofriends/src/components/Card.test.js
+++ section_6_testing/testing_robofriends/src/components/Card.test.js
@@ -3,12 +3,16 @@ import { shallow, mount, render } from "enzyme";
 import Card from "./Card";
 
 // Shallow means only render card if there's <Header /> inside card IT WON'T BE RENDERED
-it("renders card compoent", () => {
-    expect(shallow(<Card />).length).toEqual(1);
-});
+// it("renders card compoent", () => {
+//     expect(shallow(<Card />).length).toEqual(1);
+// });
 
 // Mount does full DOM rendering, use if your code uses querySelector and all that stuff
 // also usefull with lifecycle methods
 
 // Render renders child in between, it's something between shallow and mount
 // doesn't need full dom only renders to static HTML
+
+it("renders card component", () => {
+    expect(shallow(<Card />)).toMatchSnapshot();
+});
